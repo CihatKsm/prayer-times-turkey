a4835cde81bb75f0e1180afbcadbe31456a1a30e
Cihat Kösem <44941726+CihatKsm@users.noreply.github.com>
2023-04-04 21:02:51 +0300

    auto update information disabled

--- prayer.js
+++ prayer.js
@@ -2,14 +2,3 @@ const { default: axios } = require('axios');
 const api = require('./api');
 
 module.exports.times = async (datas) => await api(datas);
-
-setTimeout(async () => await checkUpdate(), 1000);
-async function checkUpdate() {
-    const package = require('./package.json');
-    const url = 'https:unpkg.com/prayer-times-turkey@latest';
-    const api = await axios({ method: 'get', url }).catch((e) => null);
-    const latest = api.request.path.split('/')[1].split('@')[1] || 0;
-
-    if (Number(latest.split('.').join('')) > Number(package.version.split('.').join('')))
-        console.log('\x1b[32m%s\x1b[0m', `✅ Please update prayer-times-turkey module ${package.version} to ${latest} version.`)
-}
