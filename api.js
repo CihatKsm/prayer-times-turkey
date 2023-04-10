49847464c3e060159e9d96999dae6629685ccaf0
Cihat Kösem <44941726+CihatKsm@users.noreply.github.com>
2023-04-10 10:40:29 +0300

    🛐 Veri alış biçimi yeniden yapılandırıldı.

--- api.js
+++ api.js
@@ -1,49 +1,53 @@
 const { default: axios } = require("axios");
-const url = (place) => `https://www.sabah.com.tr/${place}-namaz-vakitleri`;
+const cheerio = require('cheerio');
 
 module.exports = async (data) => {
+    const places = require('./places').map(m => { return { name: m.name.toLowerCase(), plate: m.plate } });
+    let place;
     if (!data?.place && !data?.plate) return null;
-    let _place;
-    const places = require('./places').map(m => { return { name: nameFix(m.name), plate: m.plate } });
-    if (data?.plate) _place = places.find(f => f.plate == data?.plate);
-    if (data?.place) _place = places.find(f => f.name == nameFix(data.place));
-    if (!_place) return null;
+    if (data?.plate) place = places.find(f => f.plate == data?.plate);
+    if (data?.place) place = places.find(f => f.name == data?.place?.toLowerCase());
+    if (!place) return null;
 
-    const place = require('./places').find(f => f.plate == _place.plate);
-    const api = await axios({ method: 'get', url: url(_place.name) }).catch((e) => null);
-    const times = getTimes(api?.data);
-    const fTime = (name) => times.find(f => f.name == name).time;
+    const response = await axios({ method: 'get', url: "https://haber.com/namaz-vakitleri" }).catch((e) => null);
+    const $ = cheerio.load(response.data);
+    let datas = [];
 
-    const minute = (x) => x.split(':').map(m => Number(m)).reduce((a, b) => a * 60 + b)
-    const rtc = (x) => x < 0 ? true : (Math.floor(x / 60) > 0 ? Math.floor(x / 60) + ' saat ' : '') + x % 60 + ' dakika kaldı.'
-
-    const iftarStatus = minute(now()) > minute(fTime('İmsak')) ? rtc(minute(fTime('Akşam')) - minute(now())) : true;
-
-    const sahurStatus = minute(now()) > minute(fTime('Akşam')) || minute(now()) < minute(fTime('İmsak')) ?
-        minute(fTime('İmsak')) - minute(now()) > 0 ?
-            rtc(minute(fTime('İmsak')) - minute(now())) :
-            rtc(((24 * 60) - minute(now())) + minute(fTime('İmsak'))) : false;
-
-    const iftar = { name: 'İftar', time: iftarStatus ? iftarStatus : true }
-    const sahur = { name: 'Sahur', time: sahurStatus ? sahurStatus : false }
-    const remainingTimes = [iftar, sahur]
-    return { place, times, remainingTimes };
-};
-
-function nameFix(_name) {
-    let name = String(_name)?.toLowerCase() || null;
-    const fixs = [['i̇', 'i'], ['ı', 'i'], ['ğ', 'g'], ['ü', 'u'], ['ş', 's'], ['ö', 'o'], ['ç', 'c']]
-    for (let fix of fixs) name = name?.replaceAll(fix[0], fix[1])
-    return name;
+    $('tr').slice(1).each((i, e) => {
+        const text = (x) => $(e).find('td').eq(x).text();
+        datas.push({
+            place: { name: text(0), plate: require('./places').find(f => f.name == text(0))?.plate },
+            times: [
+                { name: 'İmsak', time: text(1) },
+                { name: 'Güneş', time: text(2) },
+                { name: 'Öğle', time: text(3) },
+                { name: 'İkindi', time: text(4) },
+                { name: 'Akşam', time: text(5) },
+                { name: 'Yatsı', time: text(6) },
+            ],
+            remainingTimes: [
+                { name: 'İftar', time: iftarTime(text(1), text(5)) },
+                { name: 'Sahur', time: sahurTime(text(1), text(5)) },
+            ]
+        })
+    })
+    
+    return datas.find(f => f.place.plate == place.plate)
 }
 
-function getTimes(html, x = '<div class="vakitler boxShadowSet">', y = '</div>') {
-    const a = html?.slice(html.indexOf(x) + x.length)
-    const b = a?.slice(0, a.indexOf(y))
-    const c = b?.split('\r\n')?.filter(f => f.includes('<strong>') || f.includes('<span>')).map(m => m.trim())?.join('')
-    const d = c?.replaceAll('</strong><span>', ':')?.replaceAll('</span><strong>', '|')?.replaceAll('<strong>', '')?.replaceAll('</span>', '')
-    const times = d?.split('|')?.map(m => m.split(':'))?.map(m => { return { name: m[0], time: m[1]+':'+m[2] } }) || [];
-    return times;
+const minute = (x) => x.split(':').map(m => Number(m)).reduce((a, b) => a * 60 + b);
+const rtc = (x) => x < 0 ? true : (Math.floor(x / 60) > 0 ? Math.floor(x / 60) + ' saat ' : '') + x % 60 + ' dakika kaldı.';
+
+function iftarTime(imsak, aksam) {
+    const iftarStatus = minute(now()) > minute(imsak) ? rtc(minute(aksam) - minute(now())) : true;
+    return iftarStatus ? iftarStatus : true;
+}
+
+function sahurTime(imsak, aksam) {
+    const sahurStatus = minute(now()) > minute(aksam) || minute(now()) < minute(imsak) ?
+        minute(imsak) - minute(now()) > 0 ? rtc(minute(imsak) - minute(now())) :
+            rtc(((24 * 60) - minute(now())) + minute(imsak)) : false;
+    return sahurStatus ? sahurStatus : false;
 }
 
 function now() {
